7f043e7ef946b1b47eb71fd25ef97cd901a89f19
zhongqing <834103033@qq.com>
2019-01-13 16:41:47 +0800

    change value controller

--- CoreApi/Controllers/ValuesController.cs
+++ CoreApi/Controllers/ValuesController.cs
@@ -9,58 +9,9 @@ namespace CoreApi.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public IAcationResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return View();
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
-	//Get api/values
-	[HttpGet("{id}")]
-	public int Get(int id)
-	{
-		return id + 100;
-	}
-
-	public void Test()
-	{
-		Console.WriteLine("Ok");
-	}
-	public void test1(){
-	{	
-		Console.WriteLine("OK");
-	}
-
-	public void test3()
-	{
-		Console.WriteLine("Test3")
-	}
-
     }
 }
